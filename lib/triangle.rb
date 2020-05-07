f5db5817961e637005c470372939c558c388f485
Adam Spencer <aspencer0919@gmail.com>
2020-05-07 23:14:29 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,10 @@
 class Triangle
-  # write code here
+
+  attr_accessor :side_1, :side_2, :side_3
+
+  def initialize(:side_1, :side_2, :side_3)
+    @side_1 = :side_1 .to_i
+    @side_2 = :side_2 .to_i
+    @side_3 = :side_3 .to_i
+  end
 end
