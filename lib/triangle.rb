50b0ff982ce1b0e242c6e0c9e47bf67228fed29f
Adam Spencer <aspencer0919@gmail.com>
2020-05-07 23:27:17 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,7 +14,13 @@ class Triangle
     elsif (@side_1+@side_2 <= @side_3) || (@side_2+@side_3 <= @side_1) || (@side_1+@side_3 <= @side_2)
       raise TriangleError
     else
-
+      if (@side_1 == @side_2) && (@side_2 == @side_3)
+        :equilateral
+      elsif (@side_1 == @side_2) || (@side_2 == @side_3) || (@side_1 == @side_3)
+        :isosceles
+      elsif (@side_1 != @side_2) && (@side_2 != @side_3) && (@side_1 != @side_3)
+        :scalene
+      end
     end
   end
 
