f72d497907430611b8c65690b68d7e48f02597c5
Adam Spencer <aspencer0919@gmail.com>
2020-05-07 23:21:21 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,14 +9,14 @@ class Triangle
   end
 
   def kind
-    if side_1 || side_2 || side_3 <= 0
+    if (side_1 <= 0) || (side_2 <= 0) || (side_3 <= 0)
       begin
         raise TriangleError
       rescue TriangleError => error
           puts error.message
       end
     else
-      person.partner = self
+      
     end
 
   end
