cb8d3a03bd715af14fe37d74f1469d3266faa34c
Adam Spencer <aspencer0919@gmail.com>
2020-05-07 23:24:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,20 +10,14 @@ class Triangle
 
   def kind
     if (@side_1 <= 0) || (@side_2 <= 0) || (@side_3 <= 0)
-      begin
-        raise TriangleError
-      rescue TriangleError => error
-          puts error.message
-      end
+      raise TriangleError
+    elsif (@side_1+@side_2 <= @side_3) || (@side_2+@side_3 <= @side_1) || (@side_1+@side_3 <= @side_2)
+      raise TriangleError
     else
-
+      
     end
-
   end
 
   class TriangleError < StandardError
-    def message
-      "These are invalid dimensions for a triangle."
-    end
   end
 end
